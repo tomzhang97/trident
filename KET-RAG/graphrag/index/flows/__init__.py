461b2b13bc1774f41a614f12c6d6c8c8b8ab231a
tomzhang97 <tomzhang021@gmail.com>
2025-12-10 15:44:32 +0800

    move baseline to folder

--- KET-RAG/graphrag/index/flows/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) 2024 Microsoft Corporation.
-# Licensed under the MIT License
-
-"""Core workflows without DataShaper wrappings."""
--- KET-RAG/graphrag/index/operations/embed_graph/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024 Microsoft Corporation.
-# Licensed under the MIT License
-
-"""The Indexing Engine graph embed package root."""
-
-from .embed_graph import EmbedGraphStrategyType, embed_graph
-from .typing import NodeEmbeddings
-
-__all__ = ["EmbedGraphStrategyType", "NodeEmbeddings", "embed_graph"]
